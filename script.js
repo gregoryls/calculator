876a38324e1b701f28fc7a31ace1c52935d68c9e
Greg Schneider <greg53x@msn.com>
2022-04-05 19:06:17 -0600

    function to split calculator input at chosen operator

--- script.js
+++ script.js
@@ -17,13 +17,21 @@ function operate(operator,a,b){
     if (operator === '*') return multiply(a,b);
     if (operator === '/') return divide(a,b);
 }
+
+function inputSplit(){
+    const separators = ['\\\+', '-', '\\*', '/'];
+    let testComponents = 
+        displayContent.split(new RegExp(separators.join('|'),'g'));
+    console.log(testComponents);
+}
+
 let display = document.querySelector('.display');
 let displayContent = '';
 let chosenOperator = '';
 
 const buttonNumberConversion = {one:'1', two:'2', three:'3',four:'4',five:'5',
     six:'6',seven:'7',eight:'8',nine:'9',zero:'0',dot:'.'};
-const buttonSymbolConversion = {plus:'+',minus:'-',times:'X',divide:'/'};
+const buttonSymbolConversion = {plus:'+',minus:'-',times:'*',divide:'/'};
 
 let buttons = Array.from(document.querySelectorAll('.calculator button'));
 
@@ -32,19 +40,32 @@ for (let i=0;i<(buttons.length);i++){
     buttons[i].addEventListener('click', ()=>{
         displayContent += `${buttonNumberConversion[buttonName]}`;
         if (buttonNumberConversion[buttonName] === undefined){
-            displayContent = displayContent.substring(0, displayContent.length - 9);
+            //displayContent = displayContent.substring(0, displayContent.length - 9);
+            displayContent = removeEndOfString(displayContent,9);
             displayContent += `${buttonSymbolConversion[buttonName]}`;
             chosenOperator = `${buttonSymbolConversion[buttonName]}`
         }
         display.textContent = displayContent;
     });
 };
-
+function removeEndOfString(string,characters){
+    string = string.substring(0,string.length - characters);
+    return string;
+}
 
 document.querySelector('#clear').addEventListener('click',()=>{
     displayContent = '';
     display.textContent = '0';
 })
+document.querySelector('#enter').addEventListener('click',()=>{
+    inputSplit();
+});
+
+
+testButton = document.querySelector('.test');
+testButton.addEventListener('click',()=>{
+    inputSplit();
+});
 
 
 
