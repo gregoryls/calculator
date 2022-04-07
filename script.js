9a4254004d93e6455d54c916527097ddea58eacf
Greg Schneider <greg53x@msn.com>
2022-04-06 23:37:37 -0600

    new logic for number buttons to avoid undefined entries

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function roundNumber (num,dec){
 }
 function calculation(){
     inputSplit(displayContent);
+    if (operationNumbers[1] == 0 && chosenOperator == '/'){
+        displayContent = '$3.50 to unlock divide by zero';
+        display.textContent = displayContent;
+        return;
+    }
     displayContent = 
         operate(chosenOperator,parseFloat(operationNumbers[0]),parseFloat(operationNumbers[1]));
     console.log(displayContent);
@@ -47,29 +52,38 @@ let operationNumbers = [];
 
 const separators = ['\\\+', '-', '\\*', '/'];
 const buttonNumberConversion = {one:'1', two:'2', three:'3',four:'4',five:'5',
-    six:'6',seven:'7',eight:'8',nine:'9',zero:'0',dot:'.'};
+    six:'6',seven:'7',eight:'8',nine:'9',zero:'0'};
 const buttonSymbolConversion = {plus:'+',minus:'-',times:'*',divide:'/'};
-
-let buttons = Array.from(document.querySelectorAll('.calculator button'));
-
-for (let i=0;i<(buttons.length);i++){
-    let buttonName = buttons[i].id;
-    buttons[i].addEventListener('click', ()=>{
-        displayContent += `${buttonNumberConversion[buttonName]}`;
-        if (buttonNumberConversion[buttonName] === undefined){
-            if (buttonName === 'enter') return;
-            if (displayContent.includes('+') || displayContent.includes('-') || 
-                displayContent.includes('*') || displayContent.includes('/')){
-                calculation();  
+let numberButtons = Array.from(document.querySelectorAll('.number'));
+for (let i=0;i<(numberButtons.length);i++){
+    let buttonName = numberButtons[i].id;
+        numberButtons[i].addEventListener('click', ()=>{
+            displayContent += `${buttonNumberConversion[buttonName]}`;
+            display.textContent = displayContent;
+});
+}
+// let buttons = Array.from(document.querySelectorAll('.calculator button'));
+// // Remove undefined logic.  Replace with loops over arrays based on classes 
+// // numbers and operations.  Others will be bespoke as is now.  Split operationNumbers
+// //in half by doing it on the operation button --> to check for bad decimals. 
+// for (let i=0;i<(buttons.length);i++){
+//     let buttonName = buttons[i].id;
+//     buttons[i].addEventListener('click', ()=>{
+//         displayContent += `${buttonNumberConversion[buttonName]}`;
+//         if (buttonNumberConversion[buttonName] === undefined){
+//             if (buttonName === 'enter') return;
+//             if (displayContent.includes('+') || displayContent.includes('-') || 
+//                 displayContent.includes('*') || displayContent.includes('/')){
+//                 calculation();  
                 
-            }
-            displayContent = removeEndOfString(displayContent,9);
-            displayContent += `${buttonSymbolConversion[buttonName]}`;
-            chosenOperator = buttonSymbolConversion[buttonName];
-        }
-        display.textContent = displayContent;
-    });
-};
+//             }
+//             displayContent = removeEndOfString(displayContent,9);
+//             displayContent += `${buttonSymbolConversion[buttonName]}`;
+//             chosenOperator = buttonSymbolConversion[buttonName];
+//         }
+//         display.textContent = displayContent;
+//     });
+// };
 
 
 document.querySelector('#clear').addEventListener('click',()=>{
@@ -81,6 +95,15 @@ document.querySelector('#enter').addEventListener('click',()=>{
     calculation();
 });
 
+dotButton = document.querySelector('#dot');
+dotButton.addEventListener('click',()=>{
+    if (displayContent.includes('.')){
+        dotButton.disabled = true;
+    }
+    displayContent = removeEndOfString(displayContent,9);
+    displayContent += '.';
+    display.textContent = displayContent;
+});
 
 backspaceButton = document.querySelector('.test');
 backspaceButton.addEventListener('click',()=>{
